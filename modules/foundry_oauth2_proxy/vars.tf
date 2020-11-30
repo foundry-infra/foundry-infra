b40ac92372502bb161f702d98e1e817b1a6d2cc7
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2020-11-30 13:58:57 -0800

    e2e works

---
+++ modules/foundry_oauth2_proxy/vars.tf
@@ -0,0 +1,18 @@
+variable "platform_provider" {
+  type = object({
+    k8s_namespace = string
+    k8s_endpoint = string
+    k8s_token = string
+    k8s_cluster_ca_certificate_b64d = string
+    digitalocean_api_token = string
+  })
+}
+
+variable "foundry_server_name" {}
+variable "workaround_subdomain_name" {}
+variable "values_yaml_path" {}
+variable "namespace" {}
+variable "role_name" {}
+variable "cluster_issuer_ref_name" {}
+variable "foundry_server_tls_secret_name" {}
+variable "foundry_hostname" {}
