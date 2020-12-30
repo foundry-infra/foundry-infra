0d0c745c4aaf42cac3186e417c9b4c1d4047341b
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2020-12-29 16:38:57 -0800

    add metrics server module and instantiate it

---
+++ modules/metrics_server/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+  }
+}
+
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "metrics-server"
+  namespace  = var.platform_provider.k8s_namespace
+}
